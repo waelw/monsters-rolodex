2f40f9716fd05e95d4b70c21cecfaccadc31f073
waelw <waelyousef656@gmail.com>
2021-07-31 08:22:37 +0300

    adding

--- src/Components/Card-list/card-list.component.jsx
+++ src/Components/Card-list/card-list.component.jsx
@@ -2,11 +2,27 @@ import React from "react"
 import {Card} from "../Card/card.component"
 import "./card-list.component.css"
 
+function sorti (a,b)
+{
+    if(a.name.charCodeAt()-b.name.charCodeAt()==0)
+    {
+        return a.name.charCodeAt(1)-b.name.charCodeAt(1)
+    }
+    else
+    return a.name.charCodeAt()-b.name.charCodeAt()
+}
 
 export const CardList = probs=>(
 
     <div className = "cardList">
-        {probs.monsters.map(monster=>{return <Card key ={monster.id} monster={monster}/>})}
+        {
+         probs.monsters.sort(sorti).map(monster=>{return <Card key ={monster.id} monster={monster}/>})} 
+
+
+
+        
+        
+       
     </div>
     
 )
